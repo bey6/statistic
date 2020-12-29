1452d47f9d5ee7539c22ebf8b8cfb2fb5914ec7a
贝 <bey.liu@outlook.com>
2020-12-29 19:15:12 +0800

    style: eslint code style fix

--- app/class/rep.js
+++ app/class/rep.js
@@ -1,16 +1,4 @@
 module.exports = class Rep {
-    /**
-     * 响应码
-     */
-    code = 200
-    /**
-     * 响应信息
-     */
-    msg = ''
-    /**
-     * 响应结果
-     */
-    data = []
     constructor({ code = 200, msg = '', data = [] }) {
         this.code = code
         this.msg = msg
